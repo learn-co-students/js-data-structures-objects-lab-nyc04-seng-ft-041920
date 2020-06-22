3f4df62fdab78d7a742b3b2e18ee70b483e1a311
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-06-22 19:57:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, val){
+
+    const newDriver = Object.assign({}, driver, {[key]: val}) 
+    //Object.assign(target, source, source)
+    //{} create empty {} then driver, new [key]:val pair will be stored in the empty {}.
+    //console.log(`:::key${key}:: val${val}::::newDriver${newDriver}`)
+    return newDriver
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, val){
+  driver[key] = val
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newDriver = Object.assign({}, driver)
+    newDriver[key] = undefined
+    return newDriver
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+
+    newDriver = Object.assign({}, driver)
+    console.log(`:::::newDriver ${newDriver}:::`)
+    driver[key] = undefined
+    
+    return newDriver
+}
